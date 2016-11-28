895cbb2f260b3e4e84afba50b8492d16ba3ef57b
Mariano Molina <xsin.crueldadx@gmail.com>
2016-11-28 16:56:10 -0300

    Update theme

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -1,15 +1,13 @@
 /**
  * This is the entry point for specific javascript of theme
  */
-$(document).ready(function() {
-    'use strict';
 
+$(document).ready(() => {
     // open menu
     $('.btn_lines').click(function(event) {
         event.preventDefault();
-        console.log(event);
+        //console.log(event);
         $(event.currentTarget).toggleClass('close');
         $('.header-top').toggleClass('visible');
     });
-
 });
